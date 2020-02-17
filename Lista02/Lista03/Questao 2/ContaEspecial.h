e165da59e8b22f737f2d6bd873e540fddc66c4ea
Diego Reis <52046178+diegolrs@users.noreply.github.com>
2020-02-17 13:47:44 -0300

    Linguagem de programação I

---
+++ Lista02/Lista03/Questao 2/ContaEspecial.h
@@ -0,0 +1,16 @@
+#ifndef CONTAESPECIAL_H
+#define CONTAESPECIAL_H
+#include "ContaEspecial.h"
+#include "Conta.h"
+
+
+class ContaEspecial : public Conta
+{
+    public:
+    ContaEspecial(Conta conta);
+    ContaEspecial();
+    void definirLimite();
+
+};
+
+#endif // CONTAESPECIAL_H
